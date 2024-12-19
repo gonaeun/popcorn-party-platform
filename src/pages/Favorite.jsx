bdc143b2554b6c44921bfbf8d344fa1c363f4dad
고나은 <gonaeunn@gmail.com>
2024-12-20 00:20:12 +0900

    찜하기 기능에 장르 안보이도록

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -1,23 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './Favorite.css';
-import tmdbApi from '../api'
 
 const Favorite = ({ userId = 1 }) => {
   const [favorites, setFavorites] = useState([]);
-  const [genreList, setGenreList] = useState([]);
 
   useEffect(() => {
-    // TMDB 장르 데이터 가져오기
-    const fetchGenres = async () => {
-      try {
-        const response = await axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key={tmdbApi}&language=ko-KR'); // YOUR_API_KEY를 실제 TMDB API 키로 교체
-        setGenreList(response.data.genres);
-      } catch (error) {
-        console.error('장르 데이터 가져오기 실패:', error);
-      }
-    };
-
     // 사용자 찜 목록 가져오기
     const fetchFavorites = async () => {
       try {
@@ -28,18 +16,8 @@ const Favorite = ({ userId = 1 }) => {
       }
     };
 
-    fetchGenres();
     fetchFavorites();
   }, [userId]);
-
-  // 숫자로 저장된 장르를 이름으로 변환
-  const getGenreNames = (genreIds) => {
-    if (!genreList.length) return [];
-    return genreIds.split(', ').map((id) => {
-      const genre = genreList.find((g) => g.id === parseInt(id, 10));
-      return genre ? genre.name : null;
-    }).filter(Boolean).join(' / '); // 장르 이름을 '/'로 연결
-  };
 
   return (
     <div className="favorite-wrapper">
@@ -56,9 +34,6 @@ const Favorite = ({ userId = 1 }) => {
             >
               <div className="favorite-overlay">
                 <h3>{movie.title}</h3>
-                <div className="genre">
-                  {getGenreNames(movie.genre)}
-                </div>
                 <div className="info">
                   <p>평점: {movie.vote_average || 0}점</p>
                 </div>
